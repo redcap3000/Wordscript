e09de77c4fe162559854eeee57d5e91de06c3158
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2012-07-06 00:33:33 -0700

    fixed sql statements to work with pagination and now uses same api calls as php/ruby versions, added notes to proceed with metadata processing, code cleanup, implemented config file

---
+++ api/config-example.node.js
@@ -0,0 +1,15 @@
+// Your  node js server information ...
+my_port = 1337;
+my_server = 'your_domain.com';
+
+// Your server host, localhost should work for local installations
+client.host ='mysql.mydb.com';
+client.user = 'mysqlUser';
+client.password = 'mysqlPass';
+
+default_fields='id,post_title,post_name,post_author,post_date,post_content,post_type,post_status';
+
+// Name of your wordpress database and prefix
+
+wp_db_name = 'your_db';
+wp_prefix = 'wp_';
--- api/wp_api.node.js
+++ api/wp_api.node.js
@@ -1,39 +1,14 @@
 // WP API Using mysql node module
 // Ronaldo Barbachano
-// May 2012
+// July 2012
 // doinglines.com 
-/*
-	Add support for post lookup by tag, single page/post listings via title.
-
-	Cleaned up flow, removed repetitive sql statements, moved variables around. SQL connections
-	no longer created per each client, sql statements no longer execute when server requests the favicon.ico
-
-
-*/ 
-
-
 
 function append_table_alias(alias,fields){return fields.replace(/,/g,',' + alias + '.');}
-
-// Your  node js server information ...
-
-my_port = 1337;
-my_server = 'your_domain.com';
-
-// Name of your wordpress database and prefix
-
-wp_db_name = 'your_db';
-wp_prefix = 'wp_';
 
 Client = require('mysql').Client;
 client = new Client(); 
 
-// Your server host, localhost should work for local installations
-client.host ='mysql.mydb.com';
-client.user = 'mysqlUser';
-client.password = 'mysqlPass';
-
-default_fields='id,post_title,post_name,post_author,post_date,post_content,post_type,post_status';
+require('./config.node.js')
 
 
 client.query('use ' + wp_db_name);
@@ -54,49 +29,53 @@ http.createServer(function (req, res) {
 	  	res.writeHead(200, {'Content-Type': 'text/plain'});
 		// get the url (this seems to run a few times..)
 	  	var url_parts = url.parse(req.url,true);
-
-	  if(req.method=='GET' && r_query == false) {
+console.log(url_parts.query)
+	  if(req.method=='GET' && r_query == false && url_parts.query.json != undefined) {
+	  		directive = url_parts.query.json.split('/')
+	  		console.log(url_parts.query)
 		// process a passed get method	
-		wp_query =  'select ' + default_fields + ' from ' + wp_prefix + 'posts where post_status ="publish"';
-			if(typeof(url_parts.query.category) != 'undefined'){
-				wp_query = "SELECT "+append_table_alias('posts',default_fields)+", term.name as category FROM "+wp_prefix+"posts posts join "+wp_prefix+"term_relationships rel on posts.id = rel.object_id join "+wp_prefix+"term_taxonomy tax on tax.term_taxonomy_id = rel.term_taxonomy_id join "+wp_prefix+"terms term on term.term_id = tax.term_id where posts.post_status = 'publish' and posts.post_type = 'post' and slug = '"+url_parts.query.category+"' order by posts.post_date desc LIMIT 10;";
-				// switch query to whatever the category is equal to
-			}else if(typeof(url_parts.query.tag) != 'undefined'){
-				// test this ... 
-				wp_query = "SELECT " + default_fields + ",posts.id FROM "+ wp_prefix +"posts posts join "+ wp_prefix +"term_relationships rel on posts.id = rel.object_id join "+ wp_prefix +"term_taxonomy tax on tax.term_taxonomy_id = rel.term_taxonomy_id join "+ wp_prefix +"terms term on term.term_id = tax.term_id WHERE posts.post_status = 'publish' and posts.post_type = 'post' and taxonomy = 'post_tag' and slug='" + url_parts.query.tag + "' GROUP BY posts.id ;";
-				// get a tag ..
-			}else if(typeof(url_parts.query.post) != 'undefined'){
-				// get one post, probably do some kind of sanitization
-				wp_query += ' and post_type="post" and post_name= "' + url_parts.query.post + '" LIMIT 1;';
-			}else if(typeof(url_parts.query.page) != 'undefined'){
-				// get one page
-				wp_query += ' and post_type="page" and post_name= "' + url_parts.query.page + '" LIMIT 1;';
-			}else if(typeof(url_parts.query.categories) != 'undefined'){
-				// get category listings
-			}else{
-				wp_query += ' and post_type="post" order by post_date desc LIMIT 10';
-			
-			}
 		
-		}else{
-			//console.log('no get method');
-			wp_query += 'post_type="post" order by post_date desc LIMIT 10';
-		}
+			if(directive[1] != undefined){
+				wp_query =  'SELECT ' + default_fields + ' FROM ' + wp_prefix + 'posts WHERE post_status ="publish"';
+				
+			
+		
+				if(directive[0] != 'posts' && directive[0] != 'tag' && directive[0] != 'page' && directive[0] != 'post' && directive[0] != 'posts'){
+				// process a category
+					wp_query = "SELECT "+append_table_alias('posts',default_fields)+", term.name AS category FROM "+wp_prefix+"posts posts JOIN "+wp_prefix+"term_relationships rel ON posts.id = rel.object_id JOIN "+wp_prefix+"term_taxonomy tax ON tax.term_taxonomy_id = rel.term_taxonomy_id JOIN "+wp_prefix+"terms term ON term.term_id = tax.term_id WHERE posts.post_status = 'publish' AND posts.post_type = 'post' AND slug = '"+ directive[0]+"' ORDER BY posts.post_date DESC LIMIT " + (directive[1]!= undefined ? directive[1] : "10") + (directive[2] != undefined ? ' OFFSET ' + directive[2] : '') + ';';
+					// switch query to whatever the category is equal to
+				}else if(directive[0] == 'tag'){
+					// test this ... 
+					wp_query = "SELECT " + default_fields + ",posts.id FROM "+ wp_prefix +"posts posts JOIN "+ wp_prefix +"term_relationships rel ON posts.id = rel.object_id JOIN "+ wp_prefix +"term_taxonomy tax ON tax.term_taxonomy_id = rel.term_taxonomy_id JOIN "+ wp_prefix +"terms term ON term.term_id = tax.term_id WHERE posts.post_status = 'publish' AND posts.post_type = 'post' AND taxonomy = 'post_tag' AND slug='" + directive[1] + "' GROUP BY posts.id LIMIT " + (directive[2]!= undefined? directive[2]:'10') + (directive[3] != undefined? ' OFFSET ' + directive[3] : '') +';';
+					// get a tag ..
+				}else if(directive[0] == 'post'){
+					// get one post, probably do some kind of sanitization
+					wp_query += ' AND post_type="post" AND post_name= "' + directive[1] + '" LIMIT 1;';
+				}else if(directive[0] == 'page'){
+					// get one page
+					wp_query += ' AND post_type="page" AND post_name= "' + directive[1] + '" LIMIT 1;';
+				}else if(directive[0] == 'categories'){
+					// get category listings
+				}else if(directive[0] == 'posts'){
+					wp_query += ' AND post_type="post" ORDER BY post_date DESC LIMIT ' + directive[1] + (directive[2] != undefined? ' OFFSET ' + directive[2] : '')+ ';';
+				}
+			}
 	
 	    if(wp_query != '' && r_query == false){
-	 
+		    console.log(wp_query);
 		client.query(
 	        wp_query,
 	        function selectCb(err, results, fields) {
 	            if (err) {
 	                console.log("ERROR: " + err.message);
-	                throw err;
+	                //throw err;
 	            }else if (r_query == false){
 	            console.log("Got "+results.length+" Rows:"   + '\n' + wp_query);
 	
 	            for(var i in results){
+		        //  meta = [];
 				// TAG lookup
-					client.query("select name as tag from "+ wp_prefix+"posts posts join "+ wp_prefix+"term_relationships rel on posts.id = rel.object_id join "+ wp_prefix+"term_taxonomy tax on tax.term_taxonomy_id = rel.term_taxonomy_id join "+ wp_prefix+"terms term on term.term_id = tax.term_id where posts.post_status = 'publish' and posts.post_type = 'post'  and post_name='"+ results[i].post_name+"' and taxonomy = 'post_tag'",
+					client.query("SELECT name AS tag FROM "+ wp_prefix+"posts posts JOIN "+ wp_prefix+"term_relationships rel ON posts.id = rel.object_id JOIN "+ wp_prefix+"term_taxonomy tax ON tax.term_taxonomy_id = rel.term_taxonomy_id JOIN "+ wp_prefix+"terms term ON term.term_id = tax.term_id WHERE posts.post_status = 'publish' and posts.post_type = 'post'  and post_name='"+ results[i].post_name+"' and taxonomy = 'post_tag'",
 					function procInnerResult(err,results2,fields){
 						var temp_tags = [];
 						for(x=0;x<results2.length;x++){
@@ -104,11 +83,28 @@ http.createServer(function (req, res) {
 						}
 						tags.push(temp_tags);
 					});
+					
+					// build array and assemble them in one call instead of looping them over
+					/*
+					console.log("SELECT guid,meta_key,meta_value FROM wp_posts JOIN "+wp_prefix+"postmeta ON "+wp_prefix+"posts.id = "+wp_prefix+"postmeta.post_id WHERE post_id=" + results[i]['id'] + " AND meta_key ='_wp_attachment_metadata' LIMIT 1");
+					
+						
+					client.query("SELECT guid,meta_key,meta_value FROM wp_posts JOIN "+wp_prefix+"postmeta ON "+wp_prefix+"posts.id = "+wp_prefix+"postmeta.post_id WHERE post_id=" + results[i]['id'] + " AND meta_key ='_wp_attachment_metadata' LIMIT 1",
+					function procInnerResult(err,results3,fields){
+						for(x=0;x<results3.length;x++){
+							console.log(results3[x]);
+							meta.push(results3[x]);
+						}
+					});	
+					*/
+					
 		     	}
 				for(z=0;z< results.length;z++){
 					results[z].tags = tags[z];
+//					results[z].meta = meta[z];
 				}
-			
+		
+//				results[z].meta = meta;
 				res.write(JSON.stringify(results));
 				res.end();
 			
@@ -118,6 +114,6 @@ http.createServer(function (req, res) {
 		}
 	}
 }
-
+}
 ).listen(my_port, my_server);
 console.log('Server running at ' + my_server+ ':' + my_port);
